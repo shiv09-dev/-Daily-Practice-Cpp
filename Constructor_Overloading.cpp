d1dd9231ee08ec55e7c806d53bacb97e29a083f5
shiv09-dev <daveshivam09@gmail.com>
2025-07-23 10:46:32 +0530

    Add files via upload

---
+++ Constructor_Overloading.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Person
+{
+	int id, contact_no;
+	string name,address;
+	
+	public:
+			Person()
+			{
+				cout<<"This constructor has no value , its just printing the sentence "<<endl;	
+				id = 0;
+				contact_no = 0;
+				name = "NULL";
+				address = "NULL";
+			}
+			
+			Person(int id1, int c_no)
+			{
+				id = id1;
+				contact_no = c_no;	
+			}
+			
+			Person(int id1, string name1, string address1)
+			{
+				id = id1;
+				name = name1;
+				address = address1;	
+			}
+			
+			void display()
+			{
+				cout<<id<<"\t"<<contact_no<<"\t"<<name<<"\t"<<address<<"\t"<<endl;	
+			}	
+};
+
+
+int main()
+{
+	Person p;
+	
+	Person p1(01,23456);
+	p1.display();
+	
+	Person p2(02,"Shivam" , "Ahmedabad");
+	p2.display();
+}
